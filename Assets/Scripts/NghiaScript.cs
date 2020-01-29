fa690c43cc7d5963b24de0b068b7abce6ffaba4e
soulburn96 <soulburn96@gmail.com>
2020-01-30 00:36:23 +0200

    following script

---
+++ Assets/Scripts/NghiaScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NghiaScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public NavMeshAgent agent;
+    public GameObject player;
+    float attackRange = 5f;
+    void Start()
+    {
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (Vector3.Distance(transform.position, player.transform.position) < attackRange)
+        {
+            
+        }
+
+        else {
+            agent.SetDestination(player.transform.position);
+        }
+    }
+}
