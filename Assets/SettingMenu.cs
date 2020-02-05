78f48a8849a70c04d620bda5f48655105084bca3
soulburn96 <soulburn96@gmail.com>
2020-02-05 23:51:27 +0200

    Main Menu

---
+++ Assets/SettingMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SettingMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Slider volumeSlider;
+    public AudioSource audioSrc;
+
+
+    public void OnEnable()
+    {
+        volumeSlider.onValueChanged.AddListener(delegate { SetVolume(); });
+    }
+    public void SetVolume()
+    {
+        audioSrc.volume = volumeSlider.value;
+
+    }
+}
